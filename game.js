7f4c6420e5c9ecd604377c260d4065f297ecb4dd
gonnavis <gonnavis@163.com>
2018-11-26 15:37:04 +0800

    根据 https://github.com/myemperor/threejsWithWechatGame/issues/1 楼主的方法, 解决 document.createElementNS not a function 报错问题

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
+import './js/libs/weapp-adapter'
+import './js/libs/symbol'
 import game3d from './3dgame'
-window.canvas = wx.createCanvas()
-window.image = wx.createImage()
+// window.canvas = wx.createCanvas()
+// window.image = wx.createImage()
 new game3d()
